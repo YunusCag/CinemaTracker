2becc2917f67ba061213bcc574d35e49697aae03
Yunus Çağlıyan <yunuscagliyan97@gmail.com>
2023-07-05 12:13:59 +0300

    ApiConstant created.

---
+++ CinemaTracker/Core/Util/ApiConstant.swift
@@ -0,0 +1,15 @@
+//
+//  AppPrivateConstant.swift
+//  CinemaTracker
+//
+//  Created by Yunus Çağlıyan on 4.07.2023.
+//
+
+import Foundation
+
+
+
+
+
+
+
---
+++ CinemaTracker/Core/Util/AppConstant.swift
@@ -0,0 +1,19 @@
+//
+//  AppConstant.swift
+//  CinemaTracker
+//
+//  Created by Yunus Çağlıyan on 5.07.2023.
+//
+
+import Foundation
+
+
+enum AppConstant: String {
+    case ADMOB_ID = "s"
+}
+
+enum ApiConstant: String {
+    case BASE_URL = "https://api.themoviedb.org/3/"
+    case BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w342"
+    case API_KEY = "4ceba0985010b11eb871640206d56895"
+}
